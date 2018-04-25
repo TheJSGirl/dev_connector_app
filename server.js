a5870c61102a49dde60ed53e24a442873cc12009
Priyanka Negi <prinegi100@gmail.com>
2018-04-25 14:20:21 +0530

    created dummy routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 
 const app = express();
 
@@ -17,6 +21,11 @@ mongoose.connection.once('open', () => {
 app.get('/', (req, res) => {
   res.send('hello');
 });
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
