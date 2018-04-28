819f6e36b74007389452134b24d21afe676050d2
Priyanka Negi <prinegi100@gmail.com>
2018-04-29 00:57:11 +0530

    profile schema

---
+++ models/Profile.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  handle: {
+    type: String,
+    trim: true,
+  },
+  company: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  status: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experinces: [
+    {
+      title: {
+        type: String,
+      },
+      company: {
+        type: String,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+      },
+      degree: {
+        type: String,
+      },
+      fieldofstudy: {
+        type: String,
+      },
+      from: {
+        type: Date,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedIn: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('profile', ProfileSchema);
