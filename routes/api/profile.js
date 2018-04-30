2166e38344116a2915e7fa776e19669b425dc999
Priyanka Negi <prinegi100@gmail.com>
2018-04-30 23:31:49 +0530

    DELETE api/profile/experience/:exp_id & api/profile/education/:edu_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -240,4 +240,60 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
       res.status(400).json(err);
     });
 });
+
+/**
+ * @route DELETE api/profile/experience/:exp_id
+ * @desc DELETE experience from profile
+ * @access Private
+ */
+
+router.delete(
+  '/experience/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        // Get remove index
+        const removeIndex = profile.experiences.map(item => item.id).indexOf(req.params.exp_id);
+
+        // Splice out of array
+        profile.experiences.splice(removeIndex, 1);
+
+        // save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+);
+
+/**
+ * @route DELETE api/profile/education/:edu_id
+ * @desc DELETE education from profile
+ * @access Private
+ */
+
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        // Get remove index
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+);
 module.exports = router;
