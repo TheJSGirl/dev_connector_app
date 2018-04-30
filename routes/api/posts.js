a2a534eed631f220f9bd4984e925fc347a7ab780
Priyanka Negi <prinegi100@gmail.com>
2018-05-01 00:11:44 +0530

    made post schema & POST /api/posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Post = require('../../models/Post');
 
 const router = express.Router();
 
@@ -8,5 +11,24 @@ const router = express.Router();
  * @access Public
  */
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+
+/**
+ * @route POST api/posts/
+ * @desc create post
+ * @access Private
+ */
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id,
+  });
+
+  newPost
+    .save()
+    .then(post => res.status(200).json(post))
+    .catch(err => res.json(err));
+});
 
 module.exports = router;
